9ee377dbbf8aaa197c1bd32ca8da54f29ca178c1
greyglamsjp <trquan@glamscorp.jp>
2023-07-09 21:12:10 +0700

    update mtc

--- foreground.js
+++ foreground.js
@@ -2,7 +2,7 @@ var sourceSelect = "vipbsn";
 var modeSelect = "0";
 var localPostUrl = "http://localhost/qbook/api/create.php";
 var postUrl = "https://tukulab.com/qbook/api/create.php";
-
+var timeoutSecond = 4500;
 chrome.storage.local.get(
   ["enabled", "sourceSelect", "modeSelect", "manualMode"],
   (data) => {
@@ -17,19 +17,15 @@ chrome.storage.local.get(
       //it is enabled, do accordingly
       setTimeout(function () {
         if (sourceSelect == "vipbsn") {
-          var chapter = document.getElementsByClassName("webkit-chapter")[0];
-          console.log("chapter " + chapter);
+          // var chapter = document.getElementsByClassName("webkit-chapter")[0];
+          var chapter = getElementByXpath("//*[@id='chapter-id']/div/span");
           var title = document.getElementsByClassName("chapter-title")[0];
-          console.log(title.innerText);
-          console.log("title " + title);
           var book = getElementByXpath(
             "//*[@id='box-chapter-content']/div/div[2]/div[1]/div/div/nav/ol/li[3]/a"
           );
-          console.log("book " + book.innerText);
           var bookAuthor = getElementByXpath(
             '//*[@id="id_chap_content"]/ul/li[2]/p'
           ).innerText;
-          console.log("bookAuthor " + bookAuthor);
           var btnBuyChapter = document.getElementsByClassName("btn-buy")[0];
           if (btnBuyChapter) {
             console.log("chưa mua chương");
@@ -39,6 +35,12 @@ chrome.storage.local.get(
               var titleText = title.innerText;
               var chapterText = chapter.innerHTML;
               var chapterNo = parseInt(textbetween(titleText, "Chương", ":"));
+              // console.log("title " + titleText);
+              console.log("chapter " + chapterText);
+              // console.log("chapter No " + chapterNo);
+              // console.log("book name " + bookName);
+              // console.log("Book author " + bookAuthor);
+
               insertTexttoDb(
                 titleText,
                 chapterText,
@@ -50,7 +52,44 @@ chrome.storage.local.get(
             }
           }
         }
-      }, 3000);
+        if (sourceSelect == "mtc") {
+          var book = getElementByXpath("/html/head/title");
+          console.log(book);
+          var title = getElementByXpath('//*[@id="js-read__body"]/div[2]');
+          var chapter = getElementByXpath('//*[@id="article"]');
+          if (chapter && title && book) {
+            var bookAuthor = "mtc";
+            var bookName = book.innerText.split(" -")[0];
+            var titleText = title.innerHTML;
+            var chapterText = chapter.innerHTML;
+            var totalDivsInChapter = occurrences(chapterText, "</div>");
+            // console.log(totalDivsInChapter);
+            if (totalDivsInChapter > 0) {
+              for (i = 0; i < totalDivsInChapter; i++) {
+                var adsText = textbetween(chapterText, "<div", "</div>");
+                adsText = "<div" + adsText + "</div>";
+                // console.log(adsText);
+                newChapterText = chapterText.replace(adsText, "");
+                chapterText = newChapterText;
+              }
+            }
+            var chapterNo = parseInt(book.innerText.split("- Chương ")[1]);
+            console.log("title " + titleText);
+            console.log("chapter " + chapterText);
+            console.log("chapter No " + chapterNo);
+            console.log("Book Name " + bookName);
+            console.log("Book author " + bookAuthor);
+            insertTexttoDbMtc(
+              titleText,
+              chapterText,
+              chapterNo,
+              bookName,
+              modeSelect,
+              bookAuthor
+            );
+          }
+        }
+      }, timeoutSecond);
     } else {
       //it is disabled
     }
@@ -76,10 +115,38 @@ function insertTexttoDb(
     if (data.status == 200) {
       //  trigger next button
       if (modeSelect == 1) {
-        let x = getElementByXpath(
+        let btnNextBns = getElementByXpath(
           "//*[@id='box-chapter-content']/div/div[2]/div[1]/div/div/div[2]/div[1]/div[2]/a"
         );
-        x.click();
+        btnNextBns.click();
+      }
+    }
+  });
+}
+
+function insertTexttoDbMtc(
+  titleText,
+  chapterText,
+  chapterNo,
+  bookName,
+  modeSelect,
+  bookAuthor
+) {
+  postData(postUrl, {
+    chapter_title: titleText,
+    chapter_content: chapterText,
+    chapter_no: chapterNo,
+    book_name: bookName,
+    book_author: bookAuthor,
+  }).then((data) => {
+    console.log(data.status);
+    if (data.status == 200) {
+      //  trigger next button
+      if (modeSelect == 1) {
+        let btnNextMtc = getElementByXpath(
+          "/html/body/div[1]/div/div/main/div[3]/div[6]/div[1]/a[2]"
+        );
+        btnNextMtc.click();
       }
     }
   });
@@ -129,4 +196,22 @@ function textbetween(s, prefix, suffix) {
     }
   }
   return s;
+}
+function occurrences(string, subString, allowOverlapping) {
+  string += "";
+  subString += "";
+  if (subString.length <= 0) return string.length + 1;
+
+  var n = 0,
+    pos = 0,
+    step = allowOverlapping ? 1 : subString.length;
+
+  while (true) {
+    pos = string.indexOf(subString, pos);
+    if (pos >= 0) {
+      ++n;
+      pos += step;
+    } else break;
+  }
+  return n;
 }
